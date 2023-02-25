c4bb3908108620aad5c203359cf0c1cd574e3a16
BOMbreakdown <125310110+BOMbreakdown@users.noreply.github.com>
2023-02-25 10:32:30 +0100

    Update streamlit_app.py
    restructuring to separate repeating code and one-off code

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,22 @@ streamlit.dataframe(fruit_to_show)
 
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+else:
+  #import requests
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  #take the json version of the response and normalise it
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  #output to the screen as a table
+  streamlit.dataframe(fruityvice_normalized)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+except URLError as e:
+  streamlit.error()
+  
 #streamlit.text(fruityvice_response.json()) #just writes the data to the screen
-
-#take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
